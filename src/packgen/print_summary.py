5536b23a20ba5403f777bacf2a00a8d397e6a61b
Fábio Pinto Fortkamp <fabio@fabiofortkamp.com>
2025-03-07 10:02:19 +0100

    Create function in summary script to calculate number ratio

--- src/packgen/print_summary.py
+++ src/packgen/print_summary.py
@@ -18,6 +18,20 @@ class Packing:
     radii: ParticlesValues
     heights: ParticlesValues
 
+    def number_ratios(self) -> ParticlesValues:
+        """Return number fraction of the packing.
+
+        Assumes unitary total mass.
+        """
+        number_ratios, _ = number_ratio(
+            mass_ratio=(self.mass_ratio_A, 1.0 - self.mass_ratio_A),
+            densities=self.densities,
+            radii=self.radii,
+            heights=self.heights,
+            total_mass=1.0,
+        )
+        return (number_ratios[0], number_ratios[1])
+
 
 def main():
     """Generate and print table."""
@@ -26,7 +40,8 @@ def main():
             mass_ratio_A=0.5, densities=(1.0, 1.0), radii=(1.0, 1.0), heights=(1.0, 1.0)
         )
     ]
-    print(packings)
+    for packing in packings:
+        print(packing.number_ratios())
 
 
 if __name__ == "__main__":
