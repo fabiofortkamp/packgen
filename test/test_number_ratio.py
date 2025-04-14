39132f554c734e2825bbe014cfa0819ff1d80136
A. Papanikolaou <andreaspap42@gmail.com>
2025-04-14 12:25:49 +0200

    Update number ratios tests

--- test/test_number_ratio.py
+++ test/test_number_ratio.py
@@ -23,65 +23,65 @@ def test_number_ratio_same_density_and_geometry():
 
     CombinationMassFractionsNormalized = [x / sum(CombinationsMassFractions) for x in CombinationsMassFractions] 
     CombinationDensities = arr.array("d", [1.0, 1.0, 1.0, 1.0])
-    TotalVolume = 30
 
-    CombinationsFractions, CombinationsPopulations = blend.number_ratio(
+    CombinationsFractions = blend.number_ratio(
         CombinationsMassFractions,
         CombinationDensities,
         CombinationsHeights,
-        CombinationsRadii,
-        TotalVolume
-    )
+        CombinationsRadii
+        )
 
-    for x, y in zip(CombinationsFractions, CombinationMassFractionsNormalized):
+    CombinationFractionsNormalized = [
+        x / sum(CombinationsFractions) for x in CombinationsFractions
+    ]
+
+    for x, y in zip(CombinationFractionsNormalized, CombinationMassFractionsNormalized):
         assert isclose(x, y, rel_tol=1e-4)
 
 
 def test_number_ratio_half_density_same_geometry():
-    """Check the resulting number ratio is the same as the mass ratio."""
+    """The resulting number ratio should have twice as many particles of the half density type."""
 
     CombinationsRadii = arr.array("d", [0.0600, 0.0600])
     CombinationsHeights = arr.array("d", [0.0500, 0.0500])
 
     CombinationsMassFractions = arr.array("d", [1, 1])
 
-    CombinationMassFractionsNormalized = [
-        x / sum(CombinationsMassFractions) for x in CombinationsMassFractions
-    ]
     CombinationDensities = arr.array("d", [0.8, 1.6])
-    TotalVolume = 30
 
-    CombinationsFractions, CombinationsPopulations = blend.number_ratio(
+    CombinationsFractions = blend.number_ratio(
         CombinationsMassFractions,
         CombinationDensities,
         CombinationsHeights,
-        CombinationsRadii,
-        TotalVolume,
-    )
+        CombinationsRadii
+        )
 
-    assert isclose(CombinationsFractions[0], 2*CombinationsFractions[1], rel_tol=1e-4)
+    assert isclose(
+        CombinationsFractions[0],
+        2 * CombinationsFractions[1],
+        rel_tol=1e-4,
+    )
 
 
 def test_number_ratio_same_density_same_mass_half_height():
-    """Check the resulting number ratio is the same as the mass ratio."""
+    """The resulting number ratio should have twice as many particles of the half height type."""
 
     CombinationsRadii = arr.array("d", [0.0600, 0.0600])
     CombinationsHeights = arr.array("d", [0.0500, 0.1000])
 
     CombinationsMassFractions = arr.array("d", [1, 1])
 
-    CombinationMassFractionsNormalized = [
-        x / sum(CombinationsMassFractions) for x in CombinationsMassFractions
-    ]
     CombinationDensities = arr.array("d", [1.6, 1.6])
-    TotalVolume = 30
 
-    CombinationsFractions, CombinationsPopulations = blend.number_ratio(
+    CombinationsFractions = blend.number_ratio(
         CombinationsMassFractions,
         CombinationDensities,
         CombinationsHeights,
-        CombinationsRadii,
-        TotalVolume,
+        CombinationsRadii
     )
 
-    assert isclose(CombinationsFractions[0], 2 * CombinationsFractions[1], rel_tol=1e-4)
+    assert isclose(
+        CombinationsFractions[0],
+        2 * CombinationsFractions[1],
+        rel_tol=1e-4,
+    )
